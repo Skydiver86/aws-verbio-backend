b365fea25b99c8ad56b95281f339aa0d2d422113
Skydiver86 <101349094+Skydiver86@users.noreply.github.com>
2025-04-16 18:47:08 +0200

    start für aws

--- server.js
+++ server.js
@@ -97,12 +97,12 @@ dashboardcon.connect((err) => {
 });
 
 
-// s3.config für Bilder
+// s3.config für Bilderxfn
 const AWS = require('aws-sdk');
 
 AWS.config.update({
-  accessKeyId: 'AKIATQPD7DOCKPOHLVV7',
-  secretAccessKey: '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',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: 'eu-north-1'
 });
 const s3 = new AWS.S3();
